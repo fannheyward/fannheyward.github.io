a41713bc4b6c298a31c8a57d62069bfb603e9eba
Heyward Fann <heyward.fan@kikatech.com>
2018-09-13 17:25:46 +0800

    sw.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,48 @@
-const CACHE_NAME = 'fannheyward_blog_1.0.0';
-
-const URLS = [
-  // Add URL you want to cache in this list.
-  '/',
-  '/index.html',
-  '/blog/archives/',
-  '/blog/archives/index.html',
-  '/css/main.css',
-  'https://cdn.jsdelivr.net/npm/autotrack@2.4.1/autotrack.js',
-];
-
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS);
-    }).then(_ => {
-      return self.skipWaiting();
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/blog/archives/',
+        '/blog/archives/index.html',
+        '/css/main.css',
+        'https://cdn.jsdelivr.net/npm/autotrack@2.4.1/autotrack.js',
+      ]);
     })
   );
 });
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        return request;
-      } else {
-        return fetch(e.request);
-      }
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('v1').then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request).then(function(response) {
+            if (response && response.ok) {
+              cache.put(event.request, response.clone());
+            }
+            return response;
+          });
+        }
+      });
+    })
+  );
+});
+
+this.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys
+        .filter(function(key) {
+          return key.indexOf('v1') !== 0;
+        })
+        .map(function(key) {
+          return caches.delete(key);
+        })
+      );
     })
   );
 });
