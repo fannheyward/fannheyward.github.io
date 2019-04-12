f5c342e1df5ad6d2b1aa4bbaa003c61b52e3c6b2
Heyward Fann <heyward.fan@kikatech.com>
2019-04-12 09:38:05 +0800

    fix sw

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,45 @@
-this.addEventListener('install', function (event) {
+this.addEventListener("install", function(event) {
+  this.skipWaiting();
   event.waitUntil(
-    caches.open('v1').then(function (cache) {
+    caches.open("v1").then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/blog/archives/',
-        '/blog/archives/index.html',
-        '/css/main.css',
-        'https://cdn.jsdelivr.net/npm/autotrack@2.4.1/autotrack.js',
+        "/",
+        "/index.html",
+        "/blog/archives/",
+        "/blog/archives/index.html",
+        "/css/main.css",
+        "https://cdn.jsdelivr.net/npm/autotrack@2.4.1/autotrack.js"
       ]);
     })
   );
 });
 
-this.addEventListener('fetch', function (event) {
+this.addEventListener("fetch", function(event) {
   event.respondWith(
-    caches.open('v1').then(function (cache) {
-      return cache.match(event.request).then(function (response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request).then(function (response) {
-            if (response && response.ok) {
-              cache.add(event.request);
-            }
-            return response;
-          });
-        }
-      });
+    caches.open("v1").then(async function(cache) {
+      const response = await cache.match(event.request);
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request).then(function(response_1) {
+          if (response_1 && response_1.ok) {
+            cache.add(event.request);
+          }
+          return response_1;
+        });
+      }
     })
   );
 });
 
-this.addEventListener('activate', function activator(event) {
+this.addEventListener("activate", function activator(event) {
   event.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys.map(function (key) {
-        return caches.delete(key);
-      }));
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.map(function(key) {
+          return caches.delete(key);
+        })
+      );
     })
   );
 });
