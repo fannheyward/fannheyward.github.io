ff22544872b289079176c7c24b7fedfd6eaf2916
Heyward Fann <fannheyward@gmail.com>
2019-05-27 16:51:40 +0800

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,22 @@
-this.addEventListener("install", function(event) {
-  this.skipWaiting();
-  event.waitUntil(
-    caches.open("v1").then(function(cache) {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/blog/archives/",
-        "/blog/archives/index.html",
-        "/css/main.css",
-        "https://cdn.jsdelivr.net/npm/autotrack@2.4.1/autotrack.js"
-      ]);
-    })
-  );
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
+
+workbox.core.setCacheNameDetails({
+  prefix: "im.fann",
+  suffix: "v1"
 });
 
-this.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.open("v1").then(async function(cache) {
-      const response = await cache.match(event.request);
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request).then(function(response_1) {
-          if (response_1 && response_1.ok) {
-            cache.add(event.request);
-          }
-          return response_1;
-        });
-      }
-    })
-  );
-});
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
 
-this.addEventListener("activate", function activator(event) {
-  event.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.map(function(key) {
-          return caches.delete(key);
-        })
-      );
-    })
-  );
-});
+workbox.googleAnalytics.initialize();
+
+workbox.routing.registerRoute(
+  /\.(?:js|json|css)$/,
+  new workbox.strategies.CacheFirst()
+);
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  new workbox.strategies.CacheFirst()
+);
